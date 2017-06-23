c8512f0ed2446d648d0263d419df5473c93293c3
Hellen <greenlemon2176@gmail.com>
2017-06-23 18:03:34 +0300

    change ImagePickerConfiguration

--- ImageSelector/Classes/ImagePickerControllerConfiguration.swift
+++ ImageSelector/Classes/ImagePickerControllerConfiguration.swift
@@ -8,28 +8,26 @@
 import Foundation
 import PermissionsService
 
-public class ImagePickerControllerConfiguration {
+public class ImagePickerControllerConfiguration: NSObject {
     
-    class var `default`:ImagePickerControllerConfiguration {
-        
+    public class var `default`:ImagePickerControllerConfiguration {
         let configuration = ImagePickerControllerConfiguration()
-        configuration.actionSheetTitle = "Select image"
-        configuration.actionSheetMessage = "Select image from proposed sources"
-        configuration.cameraActionTitle = "Take a photo"
-        configuration.galleryActionTitle = "Chose a photo"
-        configuration.removeActionTitle = "Remove a photo"
-        configuration.cancelActionTitle = "Cancel"
         return configuration
     }
     
-    var actionSheetTitle: String?
-    var actionSheetMessage: String?
-    var cameraActionTitle: String?
-    var galleryActionTitle: String?
-    var removeActionTitle: String?
-    var cancelActionTitle: String?
-    var cameraMessages: ServiceMessages = CameraDefaultMessages()
-    var galleryMessages: ServiceMessages = GalleryDefaultMessages()
+    public var actionSheetTitle: String  = "Select image"
+    public var actionSheetMessage: String = "Select image from proposed sources"
+    public var cameraActionTitle: String = "Take a photo"
+    public var galleryActionTitle: String = "Chose a photo"
+    public var removeActionTitle: String = "Remove a photo"
+    public var cancelActionTitle: String = "Cancel"
+    public var cameraMessages: ServiceMessages = CameraDefaultMessages()
+    public var galleryMessages: ServiceMessages = GalleryDefaultMessages()
     
-    var camera: UIImagePickerControllerCameraDevice = .front
+    public var camera: UIImagePickerControllerCameraDevice = .front
+    
+    public override init() {
+        super.init()
+    }
+    
 }
